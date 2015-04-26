bd7f6ad01d65125d3639d85990f16eaf30dba580
刘德山 <shan@ubuntu.(none)>
2015-04-26 19:12:07 +0800

    add test direcory

---
+++ testcode/test/test.c
@@ -0,0 +1,40 @@
+/*
+*  system.c - Demonstrate the system() call
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+#if 0
+void  main(void)
+{
+    char *p1 = "testy";
+    char *p2 = (char *)malloc(10);
+    memset((void *)p2, 0, sizeof(p2));
+    memcpy((void *)p2, (void *)p1, strlen(p1));
+    //memcpy((void *)p2, (void *)p1, sizeof(p1));
+    printf("%d  %d %s\n", sizeof(p2), strlen(p2), p2);
+}
+#endif
+#if 0
+void  main(void)
+{
+    int x = 5;
+    printf("%d, %d, %d\n", x <<=1, x<<=2, x >>=3);
+}
+#endif
+#if 1
+void getPortStr(int portNum, char *pStr)
+{
+    sprintf(pStr, "The Port Number is: %d\r\n", portNum);
+}
+void  main(void)
+{
+    char str[24] = {0};
+     int port = 32;
+     getPortStr(port, str);
+     printf("%s", str);
+}
+#endif
+
