f1af0ca0cde726332a0db173f82eb09c4cf94cc5
Sam Liu <shan3275@gmail.com>
2017-12-13 21:18:17 +0800

    增加python网络数据采集 这本书的示例，在python-scraping目录中

---
+++ script/python/python-scraping/chapter1/2-beautifulSoup.py
@@ -0,0 +1,14 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = urlopen("http://www.pythonscraping.com/exercises/exercise1.html")
+bsObj = BeautifulSoup(html.read(), "lxml")
+print(bsObj.h1)
+print (" ")
+print(bsObj.title)
+print (" ")
+print(bsObj.head)
+print (" ")
+print(bsObj.div)
+print (" ")
+print(bsObj)
